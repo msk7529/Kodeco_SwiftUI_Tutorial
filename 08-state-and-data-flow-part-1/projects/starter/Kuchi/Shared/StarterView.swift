2bc89030e530200b30a9668af2792fa383a504c9
G.magpie <msk7529@naver.com>
2023-07-11 00:49:43 +0900

    [Chapter8] 전체 line indent 수정, 7챕터의 주석 추가

--- 08-state-and-data-flow-part-1/projects/starter/Kuchi/Shared/StarterView.swift
+++ 08-state-and-data-flow-part-1/projects/starter/Kuchi/Shared/StarterView.swift
@@ -33,20 +33,21 @@
 import SwiftUI
 
 struct StarterView: View {
-  @EnvironmentObject var userViewModel: UserManager
-  
-  var body: some View {
-    if userViewModel.isRegistered {
-      WelcomeView()
-    } else {
-      RegisterView()
+    
+    @EnvironmentObject var userViewModel: UserManager
+    
+    var body: some View {
+        if userViewModel.isRegistered {
+            WelcomeView()
+        } else {
+            RegisterView()
+        }
     }
-  }
 }
 
 struct StarterView_Previews: PreviewProvider {
-  static var previews: some View {
-    StarterView()
-      .environmentObject(UserManager())
-  }
+    static var previews: some View {
+        StarterView()
+            .environmentObject(UserManager())
+    }
 }
