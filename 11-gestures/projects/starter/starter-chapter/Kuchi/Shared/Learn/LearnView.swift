8723e068c08a6159ac40159831111b097623194e
G.magpie <msk7529@naver.com>
2023-07-14 01:43:14 +0900

    [Chapter11] 탭바에 Learn 탭 추가

--- 11-gestures/projects/starter/starter-chapter/Kuchi/Shared/Learn/LearnView.swift
+++ 11-gestures/projects/starter/starter-chapter/Kuchi/Shared/Learn/LearnView.swift
@@ -1,15 +1,15 @@
-/// Copyright (c) 2023 Kodeco Inc.
-///
+/// Copyright (c) 2023 Razeware LLC
+/// 
 /// Permission is hereby granted, free of charge, to any person obtaining a copy
 /// of this software and associated documentation files (the "Software"), to deal
 /// in the Software without restriction, including without limitation the rights
 /// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 /// copies of the Software, and to permit persons to whom the Software is
 /// furnished to do so, subject to the following conditions:
-///
+/// 
 /// The above copyright notice and this permission notice shall be included in
 /// all copies or substantial portions of the Software.
-///
+/// 
 /// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
 /// distribute, sublicense, create a derivative work, and/or sell copies of the
 /// Software in any work that is designed, intended, or marketed for pedagogical or
@@ -17,7 +17,7 @@
 /// or information technology.  Permission for such use, copying, modification,
 /// merger, publication, distribution, sublicensing, creation of derivative works,
 /// or sale is expressly withheld.
-///
+/// 
 /// This project and source code may use libraries or frameworks that are
 /// released under various Open-Source licenses. Use of those libraries and
 /// frameworks are governed by their own individual licenses.
@@ -32,56 +32,14 @@
 
 import SwiftUI
 
-struct HomeView: View {
-    
-    @EnvironmentObject var userManager: UserManager
-    @EnvironmentObject var challengesViewModel: ChallengesViewModel
-    
-    @State var selectedTab = 0
-    
+struct LearnView: View {
     var body: some View {
-        TabView(selection: $selectedTab) {
-            LearnView()
-                .tabItem({
-                    VStack {
-                        Image(systemName: "bookmark")
-                        Text("Learn")
-                    }
-                })
-                .tag(0)
-            
-            PracticeView(
-                challengeTest: $challengesViewModel.currentChallenge,
-                userName: $userManager.profile.name,
-                numberOfAnswered: .constant(challengesViewModel.numberOfAnswered)   // getter property를 @Binding 으로 넘기고자 할 때 사용
-            )
-            .tabItem({
-                VStack {
-                    Image(systemName: "rectangle.dock")
-                    Text("Challenge")
-                }
-            })
-            .tag(1)     // 해당 탭의 인덱스
-            // .environment(\.questionsPerSession, challengesViewModel.numberOfQuestions)
-            // .environment(\.verticalSizeClass, .compact) // 해당 뷰의 방향을 고정
-            
-            SettingsView()
-                .tabItem({
-                    VStack {
-                        Image(systemName: "gear")
-                        Text("Settings")
-                    }
-                })
-                .tag(2)     // 해당 탭의 인덱스
-        }
-        .accentColor(.orange)   // 탭 선택시 아이콘, 텍스트 컬러
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
-struct HomeView_Previews: PreviewProvider {
+struct LearnView_Previews: PreviewProvider {
     static var previews: some View {
-        HomeView()
-            .environmentObject(UserManager())
-            .environmentObject(ChallengesViewModel())
+        LearnView()
     }
 }
